987692143e02cfe8c3c53303bff889d036451a12
TC <xiaolanfantasy@126.com>
2017-09-19 09:50:13 +0800

    mtio/bat/filecpp/FileCpp.java

--- src/mtio/bat/filecpp/FileCpp.java
+++ src/mtio/bat/filecpp/FileCpp.java
@@ -1,5 +1,7 @@
 package mtio.bat.filecpp;
 
+import java.io.File;
+
 import mtio.bat.RunBat;
 
 public class FileCpp {
@@ -10,4 +12,44 @@ public class FileCpp {
 		return rb.result;
 	}
 
+	public void delFolder(String folderPath) {
+		try {
+			delAllFile(folderPath);
+			String filePath = folderPath;
+			filePath = filePath.toString();
+			java.io.File myFilePath = new java.io.File(filePath);
+			myFilePath.delete();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean delAllFile(String path) {
+		boolean flag = false;
+		File file = new File(path);
+		if (!file.exists()) {
+			return flag;
+		}
+		if (!file.isDirectory()) {
+			return flag;
+		}
+		String[] tempList = file.list();
+		File temp = null;
+		for (int i = 0; i < tempList.length; i++) {
+			if (path.endsWith(File.separator)) {
+				temp = new File(path + tempList[i]);
+			} else {
+				temp = new File(path + File.separator + tempList[i]);
+			}
+			if (temp.isFile()) {
+				temp.delete();
+			}
+			if (temp.isDirectory()) {
+				delAllFile(path + "/" + tempList[i]);
+				delFolder(path + "/" + tempList[i]);
+				flag = true;
+			}
+		}
+		return flag;
+	}
 }
