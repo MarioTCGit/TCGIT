4fd9a55c44d3d7ce764b7e1fb91afea4b43709e6
TC <xiaolanfantasy@126.com>
2017-09-19 09:54:25 +0800

    mtio

--- src/mtio/bat/test/Test.java
+++ src/mtio/bat/test/Test.java
@@ -6,6 +6,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		FileCpp fc = new FileCpp();
+		fc.delFolder("E:\\ADT\\cpp2");
 		System.out.println(fc.copy("E:\\APEM\\JSPlumb", "E:\\ADT\\cpp2"));
 	}
 
